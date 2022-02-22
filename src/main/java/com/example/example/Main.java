3f6d4dbb853285215cd930560a3a5315fec9e2be
Paulo Rodrigo <paulorodrigo1992@gmail.com>
2022-02-22 11:14:45 -0300

    Usando Textfield e Label

--- src/main/java/com/example/example/Main.java
+++ src/main/java/com/example/example/Main.java
@@ -1,10 +1,12 @@
 package com.example.example;
 
+import com.example.util.Alerts;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
--- src/main/java/com/example/example/ViewController.java
+++ src/main/java/com/example/example/ViewController.java
@@ -7,18 +7,37 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Locale;
 
 public class ViewController {
 
     @FXML
-    private Button btTest;
+    private TextField txtFirstNumber;
 
     @FXML
-    protected void onBtTestAction() {
+    private TextField txtSecondNumber;
 
-        Alerts.showAlerts("Alert title", null, "Hello", Alert.AlertType.INFORMATION);
+    @FXML
+    private Label labelResult;
+
+    @FXML
+    private Button btSum;
+
+    @FXML
+    protected void onBtSumAction() {
+        try {
+            Locale.setDefault(Locale.US);
+            double number1 = Double.parseDouble(txtFirstNumber.getText());
+            double number2 = Double.parseDouble(txtSecondNumber.getText());
+            double sum = number1 + number2;
+            labelResult.setText(String.format("%.2f", sum));
+        } catch (NumberFormatException e) {
+            Alerts.showAlerts("Error", "Parse Error", e.getMessage(), Alert.AlertType.ERROR);
+        }
     }
 }
